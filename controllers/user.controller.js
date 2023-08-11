c2ea0177311487decfb3934b58ae54f126cc2cf9
singhkaushik <singhkaushik28@gmail.com>
2023-08-11 17:48:39 +0530

    reg

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,65 @@
-const register = (req, res) => {};
+const register = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName || !email || !password) {
+    return next(new AppError("All fields are required", 400));
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return next(new AppError("Email already exits", 400));
+  }
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: {
+      public_id: email,
+      secure_url:
+        "https://res.cloudinary.com/du9jzqlpt/image/upload/v1674647316/avatar_drzgxv.jpg",
+    },
+  });
+
+  if (!user) {
+    return next(new AppError("User registration fail please try again", 400));
+  }
+
+  // file upload
+
+  if (req.file) {
+    try {
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "lms",
+        width: 250,
+        height: 250,
+        gravity: "faces",
+        crop: "fill",
+      });
+      if (result) {
+        user.avatar.public_id = result.public_id;
+        user.avatar.secure_url = result.secure_url;
+        //remove file from server
+        fs.rm(`upload/${req.file.filename}`);
+      }
+    } catch (e) {
+      return next(
+        new AppError(error || "file not uploaded,please try again", 500)
+      );
+    }
+  }
+  await user.save();
+  user.password = undefined;
+
+  const token = await User.generateJWTToken();
+
+  res.cookie("token", token, cookieOptions);
+
+  res.status(201).json({
+    success: true,
+    message: "User registered successfully",
+    user,
+  });
+};
 const login = (req, res) => {};
 const logout = (req, res) => {};
 const profile = (req, res) => {};
